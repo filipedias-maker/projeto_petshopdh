8cae5414db8043f7525dccee915767113dd32c51
Filipe Dias <filipe.dias09@hotmail.com>
2022-07-13 16:57:37 -0300

    main

--- funcoes.js
+++ funcoes.js
@@ -1,128 +1,73 @@
-//**PROJETO PETSHOP EM ANDAMENTO**
+//**PROJETO PETSHOP**
 
 const cachorros = require('./database/cachorros.json');
 const fs = require('fs');
 const path = require('path');
-const { splice } = require('./settings/servicos');
-
-// SALVAR
-
-function salvar(cachorro){
+let funcoes = {
+  salvar: function() {
     let arquivo = path.resolve('./database/cachorros.json');
-    let json = JSON.stringify(cachorros,null,4);
-    fs.writeFileSync(arquivo,json);
-}
-
-//**TESTE**
-//salvar({"id": 8, "nome": "Bob", "castrado": false, "dataDeNascimento": "2020-04-05", "peso": 3, "sexo": "f"});
-//console.table(cachorros);
-
-//BUSCAR
-
-function buscar(id){
+    let json = JSON.stringify(cachorros, null, 4);
+  
+    fs.writeFileSync(arquivo, json);
+  } ,
+  buscar: function(id) { 
     let cachorro = cachorros.find(cachorro => {
-        return cachorro.id == id;
-})
-if (cachorro){
-    return cachorro;
-} else {
-    return `Não existe cachorro com o id ${id}`;
-}
-
-}
-
-//**TESTE**
-//buscar (1);
-//console.log(buscar(1));
-
-// LISTAR
-
-//    console.table(cachorros);
-
-//DESCREVER
-function descrever(id){
-    let cachorro = buscar(id);
-    if (cachorro){
-        console.log(cachorro);
+       return cachorro.id == id;
+    })
+      if(cachorro) {
+        return cachorro;
+      } else {
+        return `Não existe cachorro com o id ${id}`;
+      }
+  },
+  listar: function(){
+    console.table(cachorros);
+  },
+  descrever: function(id) {
+    let cachorro = this.buscar(id);  
+  
+    if(cachorro) {
+      console.log(cachorro);
     } else {
-        console.log(`Não existe cachorro com o id ${id}`);
+      console.log(`Não existe cachorro com o id ${id}`);
     }
-
-}
-
-//**TESTE** 
-//descrever(3);
-//console.log(descrever(1));
-
-let cachorro = {
-    "nome": "Bob",
-    "sexo": "m",
-    "castrado": false,
-    "dataDeNascimento": "2020-10-12",
-    "peso": 3
- }
-
-// ADICIONAR
-
-function adicionar(cachorro){
-let novoCachorro = cachorro;
-    novoCachorro.id = cachorros.length +1;
-    novoCachorro.vacinas = [];
-    novoCachorro.servicos = [];
-    cachorros.push(novoCachorro); 
-}
-
-//**TESTE**
-//adicionar(cachorro);
-//console.table(cachorros);
-
-//Atribuir Serviço
-
-let dadosDoServico = {"nome": "Maia", "data": "2022-07-04"};
-
-function atribuicaoDeServico(id, dadosDoServico){
-    let dadosDoDog = buscar(id);
-    if (dadosDoDog.id){
-        dadosDoDog.servicos.push(dadosDoServico);
+  },
+  adicionar:function(dog) {
+    let newDog = dog;
+    let lastDog = cachorros.length;
+     newDog.id = lastDog + 1;
+     newDog.vacinas = [];
+     newDog.servicos = [];
+     cachorros.push(newDog);
+     this.salvar();
+  },
+  vacinar:function(id, dadosVacina) {
+    let dadosDoCachorro = this.buscar(id);
+  
+    if(dadosDoCachorro.id) {  
+      dadosDoCachorro.vacinas.push(dadosVacina);
     } else {
-        return "Animal inexistente!"
+      console.log("Animal inexistente");
     }
-}
-
-//atribuicaoDeServico(5,{"nome": "Banho","data": "2022-07-04"});
-//   console.table(cachorros);
-//    console.log(cachorros[4]);
-
-let vacina = {"nome": "Adenovirose", "Data": "06-07-2022"}
-
-function vacinar(id, vacina){
-    let dadosDaVacina = buscar(id);
-    if (dadosDaVacina.id){
-        dadosDaVacina.vacinas.push(vacina);
+   },
+   atribuirServico: function(id, dadosDoServico) {
+    let dadosDoCachorro = this.buscar(id);
+    if(dadosDoCachorro.id) {
+      dadosDoCachorro.servicos.push(dadosDoServico)
     } else {
-        return "Cachorro inexistente"
+      console.log("Animal inexistente");
     }
-}
-
-//vacinar(5,{"nome": "Adenovirose", "Data": "06-07-2022"});
-//console.table(cachorros);
-//console.log(cachorros[4]);
-
-let remov = {"id": 8, "nome": "Bob", "castrado": false, "dataDeNascimento": "2020-04-05", "peso": 3, "sexo": "f"}
-
-function remover(id, remov){
-    let removendoDaLista = buscar(id);
-    if (removendoDaLista.id){
-        removendoDaLista.remov.pop(remov)
+  },
+  remover: function(id) {
+    let dadosDoCachorro = this.buscar(id);
+    if(dadosDoCachorro.id){
+      let index = cachorros.findIndex(cachorro => cachorro == dadosDoCachorro);
+      cachorros.splice(index,1 );
+        this.salvar();
     } else {
-        return "Cachorro inexistente"
-    }
+        console.log("Animal inexistente");
+      }
+  }
 }
-
-//remover(8,{"id": 8, "nome": "Bob", "castrado": false, "dataDeNascimento": "2020-04-05", "peso": 3, "sexo": "f"});
-//console.table(cachorros);
-//console.log(cachorros[8]);
-
-module.exports = {
-        
-}
+  
+module.exports = funcoes;
--- teste.js
+++ teste.js
@@ -1,15 +1,18 @@
 const funcoes = require('./funcoes');
 
 console.log(funcoes.buscar(1));
-// console.log(funcoes.listar());
+
+//console.log(funcoes.listar());
 // console.log(funcoes.adicionar(
-// {"nome": "Bob", "castrado": true, "peso": 3, "sexo": "m", "dataNascimento": "2020-07-25"})
+// {"nome": "Rexi", "castrado": false, "peso": 1, "sexo": "m", "dataNascimento": "2021-07-13"})
 // );
 // console.log(funcoes.listar());
 // console.log(funcoes.descrever(5));
-// console.log(funcoes.vacinar(7, {"nome": "antirrábica", "data": "2022-07-30"}));
-// console.log(funcoes.atribuirServico(7, {"nome": "banho", "data": "2022-07-06"}));
-// console.log(funcoes.descrever(7));
+// console.log(funcoes.vacinar(5, {"nome": "antirrábica", "data": "2022-07-13"}));
+// console.log(funcoes.atribuirServico(7, {"nome": "banho", "data": "2022-07-12"}));
+// console.log(funcoes.descrever(6));
 // console.log(funcoes.listar());
-// console.log(funcoes.remover(7));
+// console.log(funcoes.remover(6));
 // console.log(funcoes.listar());
+
+
